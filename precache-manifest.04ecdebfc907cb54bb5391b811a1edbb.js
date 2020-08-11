fad54542c40c18ae6dcc31e8c9ef57eaf639ba6b
Aman Rai <arai24197@gmail.com>
2020-08-11 13:55:40 +0530

    Updates

---
+++ precache-manifest.04ecdebfc907cb54bb5391b811a1edbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff14997d49b82e91aa9ebbafa2a9852",
+    "url": "/Tooltip-React/index.html"
+  },
+  {
+    "revision": "2a3c7ea5af4111cef854",
+    "url": "/Tooltip-React/static/css/main.275cc208.chunk.css"
+  },
+  {
+    "revision": "39f2c1537d206e9168ea",
+    "url": "/Tooltip-React/static/js/2.0e2e7a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tooltip-React/static/js/2.0e2e7a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a3c7ea5af4111cef854",
+    "url": "/Tooltip-React/static/js/main.16deaae2.chunk.js"
+  },
+  {
+    "revision": "e30ccee4a1ef77f19bbb",
+    "url": "/Tooltip-React/static/js/runtime-main.44eb4a9f.js"
+  }
+]);
